87cc9c8411288d8cabe93a52e1b9d3757468db8b
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-15 23:05:26 +0900

    feat: ディレクトリ操作を追加

---
+++ playground/dir/main.go
@@ -0,0 +1,23 @@
+// ディレクトリ操作
+package main
+
+import (
+    "os"
+    "fmt"
+)
+
+func main() {
+    dir := "./testdir"
+    os.Mkdir(dir, os.ModePerm)
+    os.Chmod(dir, 0777)
+
+    recursiveMkdir(dir)
+}
+
+func recursiveMkdir(parent string) {
+    // 数字で range する
+    for i, _ := range [10]int{} {
+        dirname := fmt.Sprintf("%v/%d", parent, i + 1)
+        os.MkdirAll(dirname, os.ModePerm)
+    }
+}
