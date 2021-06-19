e8fbcff17a96e93d5905963e92256d714e033c99
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-19 21:28:51 +0900

    feat: cors 対策を追加

--- playground/open-api/restapi/configure_todo_list.go
+++ playground/open-api/restapi/configure_todo_list.go
@@ -8,6 +8,8 @@ import (
 	"sync"
 	"sync/atomic"
 
+	"github.com/rs/cors"
+
 	errors "github.com/go-openapi/errors"
 	runtime "github.com/go-openapi/runtime"
 	middleware "github.com/go-openapi/runtime/middleware"
@@ -136,5 +138,6 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	return handler
+	handleCORS := cors.Default().Handler
+	return handleCORS(handler)
 }
