fd69c0d2d4ee2d3ca902bdc221e585e42fe58096
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-19 21:32:49 +0900

    feat: backend に fetch するコードを追加

--- playground/open-api/frontend/pages/index.tsx
+++ playground/open-api/frontend/pages/index.tsx
@@ -1,7 +1,61 @@
+import aspida from "@aspida/fetch"
+import api from '../api/$api'
+import {useEffect, useState} from "react";
+import {Item} from "../api/@types";
 
+const url = 'http://127.0.0.1:3002/'
+
+const client = api(aspida(fetch, {baseURL: url}))
+
+const fetchAllTodo = async(
+  {since = 0, limit = 20}:
+    {since: number, limit: number}
+) => {
+  return await client.$get({query: {since, limit}})
+}
 
 export default function Index() {
+  const [todos, setTodos] = useState<Item[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
+  const handleClick = async () => {}
+
+  useEffect(() => {
+    (async() => {
+      setLoading(true)
+      try {
+        const todos = await fetchAllTodo({since: 0, limit: 5})
+        setTodos(todos)
+      } catch (e) {
+        setError(e)
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, [])
+
+  if (loading) {
+    return <div>loading</div>
+  }
+
+  if (error !== null) {
+    return <div>error: {error.message}</div>
+  }
+
+  if (todos.length === 0) {
+    return (
+      <>
+        <div>empty</div>
+        <button onClick={handleClick}>Add Todo</button>
+      </>
+    )
+  }
+
   return (
-    <div>aaa</div>
+    <>
+    <ul>
+      {todos.map((todo: Item, i) => (<li key={i}>{todo.id}: {todo.description}</li>))}
+    </ul>
+    </>
   )
 }
