9e0a33812243b46a8808f2dda916161593958cb4
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-20 23:37:29 +0900

    feat: gqlgen で生成したコードを更新

--- playground/graphql/backend/graph/resolver.go
+++ playground/graphql/backend/graph/resolver.go
@@ -1,8 +1,15 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen
+
+import (
+	"example.com/gqlgen-todos/graph/model"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
+	todos []*model.Todo
 }
